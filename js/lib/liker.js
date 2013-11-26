e665a175b7b4385445ceb4c4cbbdd7d1710dcc01
Richard <r.hoffmann@einwaage.de>
2013-11-26 18:06:05 +0100

    more refactoring

--- js/lib/liker.js
+++ js/lib/liker.js
@@ -2,27 +2,41 @@
  * Created by richard on 26.11.13.
  */
 
+
+// RESPONSIBILITY: hold the data and initialize
 var Liker = function(){
-  "use strict";
   this.likes = 1;
-
-  this.subscribe("liked", function(){
-    this.render();
-  });
-
   this.bind();
 };
 
+
+// RESPONSIBILITY: update the count of likes
 Liker.prototype.updateLikes = function(){
   this.likes++;
   this.publish("liked");
 };
 
-Liker.prototype.render = function(){
+
+// RESPONSIBILITY: render/update the DOM
+Liker.prototype.render = function() {
+  this.updateButton();
+  this.logEntry();
+};
+
+
+// RESPONSIBILITY: update the button
+Liker.prototype.updateButton = function() {
   $('#like').html( this.likes );
+};
+
+
+// RESPONSIBILITY: log the entry to the list
+Liker.prototype.logEntry = function() {
   $('#like-count').append('<li>' + new Date().toString() + '</li>');
 };
 
+
+// RESPONSIBILITY: bind events/subscriptions
 Liker.prototype.bind = function() {
   var self = this;
 
@@ -31,6 +45,11 @@ Liker.prototype.bind = function() {
     self.updateLikes();
   });
 
+  this.subscribe("liked", function(){
+    this.render();
+  });
 };
 
+
+// MIXIN PubSub for publish/subscribe
 Liker.mixin(PubSub);
